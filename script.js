309ffda8de2378230aa16b687e8daefba9189048
MarkKnowsNothing <marknguyen.knows.nothing@gmail.com>
2023-01-19 14:37:35 +0700

    Add score system and win announcement in the end

--- script.js
+++ script.js
@@ -3,13 +3,31 @@ function playRound(playerSelection, computerSelection) {
         return "Tie!"
     } else
     if (computerSelection === "rock") {
-        return (playerSelection === "paper") ? "You win!" : "You lose!"
+        if (playerSelection === "paper") {
+            playerPoint++
+            return "You win!"
+        } else {
+            computerPoint++
+            return "You lose!"
+        }
     } else
     if (computerSelection === "paper") {
-        return (playerSelection === "scissors") ? "You win!" : "You lose!"
+        if (playerSelection === "scissors") {
+            playerPoint++
+            return "You win!"
+        } else {
+            computerPoint++
+            return "You lose!"
+        }
     } else
     if (computerSelection === "scissors") {
-        return (playerSelection === "rock") ? "You win!" : "You lose!"
+        if (playerSelection === "rock") {
+            playerPoint++
+            return "You win!"
+        } else {
+            computerPoint++
+            return "You lose!"
+        }
     } else {
         return "Error!"
     }
@@ -29,17 +47,30 @@ function getComputerChoice() {
 }
 
 function game() {
+    playerPoint = 0
+    computerPoint = 0
+
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("ROCHAMBEAU - ROCK PAPER SCISSORS GAME || Choose your weapon (type in rock, paper or scissors). You have " + (5 - i) + " times left.").toLowerCase()
+        let playerSelection = prompt(`ROCHAMBEAU - ROCK PAPER SCISSORS GAME || Choose your weapon (type in rock, paper or scissors). You have ${5 - i} times left.`).toLowerCase()
         let computerSelection = getComputerChoice()
 
         playRound(playerSelection, computerSelection)
 
         console.log(`Round ${i + 1}:`)
-        console.log(`Player: ${playerSelection}`)
+        console.log(`You: ${playerSelection}`)
         console.log(`Computer: ${computerSelection}`)
         console.log(playRound(playerSelection, computerSelection))
-        
+        console.log(`You: ${playerPoint / 2} || Computer: ${computerPoint / 2}`)  
+    }
+
+    if (playerPoint === computerPoint) {
+        return "It's a tie game! (Press Refresh or Ctrl + R to try again)"
+    } else
+    if (playerPoint > computerPoint) {
+        return "You've won this game! Wanna try again? (Press Refresh or Ctrl + R to try again)"
+    } else
+    if (playerPoint < computerPoint) {
+        return "You've lost this game! Try again next time? (Press Refresh or Ctrl + R to try again)"
     }
 }
 
